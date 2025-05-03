7871e3aa8ecc3d8bfaa0ee6ba4b2b9730ceba6ca
Adrian Aviles <adrian8167e@gmail.com>
2025-05-03 07:12:08 -0600

    Create solution
    Create solution with main and test project including required dependencies

---
+++ Flowsy.Db.Unity.Test/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
+[assembly: TestFramework("Xunit.Extensions.Ordering.TestFramework", "Xunit.Extensions.Ordering")]
+
+[assembly: TestCollectionOrderer("Xunit.Extensions.Ordering.CollectionOrderer", "Xunit.Extensions.Ordering")]
+
+[assembly: TestCaseOrderer("Xunit.Extensions.Ordering.TestCaseOrderer", "Xunit.Extensions.Ordering")]
---
+++ README.md
@@ -0,0 +1,1 @@
+# Flowsy Db Unity
