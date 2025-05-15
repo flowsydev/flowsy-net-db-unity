52bd923c77763e187d0d7f591709aaee5a4eeb68
Adrian Aviles <adrian8167e@gmail.com>
2025-05-14 20:44:49 -0600

    Add XML docs to extension methods for executing SQL statements

--- Flowsy.Db.Unity/Extensions/DbConnectionExtensions.Statement.Get.cs
+++ Flowsy.Db.Unity/Extensions/DbConnectionExtensions.Statement.Get.cs
@@ -6,6 +6,36 @@ namespace Flowsy.Db.Unity.Extensions;
 
 public static partial class DbConnectionExtensions
 {
+    /// <summary>
+    /// Executes a database statement (SQL command) asynchronously and returns the result as an enumerable collection of type T.
+    /// </summary>
+    /// <param name="connection">
+    /// The database connection to use for executing the statement.
+    /// </param>
+    /// <param name="commandText">
+    /// The SQL command text to execute.
+    /// </param>
+    /// <param name="parameters">
+    /// The parameters to pass to the command. This can be an anonymous object or a dictionary.
+    /// </param>
+    /// <param name="transaction">
+    /// An optional transaction to use for the command execution.
+    /// </param>
+    /// <param name="conventions">
+    /// The conventions to use for the command execution. If null, the default conventions will be used.
+    /// </param>
+    /// <param name="onExecuting">
+    /// An optional action to execute before the command is executed. This can be used from services to raise events or log the command.
+    /// </param>
+    /// <param name="onExecuted">
+    /// An optional action to execute after the command is executed. This can be used from services to raise events or log the command.
+    /// </param>
+    /// <typeparam name="T">
+    /// The type to map the results to.
+    /// </typeparam>
+    /// <returns>
+    /// An enumerable collection of type T containing the result set returned by the command.
+    /// </returns>
     public static IEnumerable<T> GetFromStatement<T>(
         this IDbConnection connection,
         string commandText,
@@ -24,6 +54,39 @@ public static partial class DbConnectionExtensions
         return onExecuted is not null ? onExecuted(commandDefinition, result) : result;
     }
     
+    /// <summary>
+    /// Asynchronously executes a database statement (SQL command) asynchronously and returns the result as an enumerable collection of type T.
+    /// </summary>
+    /// <param name="connection">
+    /// The database connection to use for executing the statement.
+    /// </param>
+    /// <param name="commandText">
+    /// The SQL command text to execute.
+    /// </param>
+    /// <param name="parameters">
+    /// The parameters to pass to the command. This can be an anonymous object or a dictionary.
+    /// </param>
+    /// <param name="transaction">
+    /// An optional transaction to use for the command execution.
+    /// </param>
+    /// <param name="conventions">
+    /// The conventions to use for the command execution. If null, the default conventions will be used.
+    /// </param>
+    /// <param name="onExecuting">
+    /// An optional action to execute before the command is executed. This can be used from services to raise events or log the command.
+    /// </param>
+    /// <param name="onExecuted">
+    /// An optional action to execute after the command is executed. This can be used from services to raise events or log the command.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// An optional cancellation token for the asynchronous operation.
+    /// </param>
+    /// <typeparam name="T">
+    /// The type to map the results to.
+    /// </typeparam>
+    /// <returns>
+    /// An enumerable collection of type T containing the result set returned by the command.
+    /// </returns>
     public static async Task<IEnumerable<T>> GetFromStatementAsync<T>(
         this IDbConnection connection,
         string commandText,
--- Flowsy.Db.Unity/Extensions/DbConnectionExtensions.Statement.GetFirstOrDefault.cs
+++ Flowsy.Db.Unity/Extensions/DbConnectionExtensions.Statement.GetFirstOrDefault.cs
@@ -6,6 +6,38 @@ namespace Flowsy.Db.Unity.Extensions;
 
 public static partial class DbConnectionExtensions
 {
+    /// <summary>
+    /// Executes a database statement (SQL command) and returns the first result as type T.
+    /// If a primitive type is required (int, string, etc.), then the first column of the first row will be returned.
+    /// If no results are found, the default value of T will be returned.
+    /// </summary>
+    /// <param name="connection">
+    /// The database connection to use for executing the statement.
+    /// </param>
+    /// <param name="commandText">
+    /// The SQL command text to execute.
+    /// </param>
+    /// <param name="parameters">
+    /// The parameters to pass to the command. This can be an anonymous object or a dictionary.
+    /// </param>
+    /// <param name="transaction">
+    /// An optional transaction to use for the command execution.
+    /// </param>
+    /// <param name="conventions">
+    /// The conventions to use for the command execution. If null, the default conventions will be used.
+    /// </param>
+    /// <param name="onExecuting">
+    /// An optional action to execute before the command is executed. This can be used from services to raise events or log the command.
+    /// </param>
+    /// <param name="onExecuted">
+    /// An optional action to execute after the command is executed. This can be used from services to raise events or log the command.
+    /// </param>
+    /// <typeparam name="T">
+    /// The type to map the result to.
+    /// </typeparam>
+    /// <returns>
+    /// The first result of the command execution as an instance of type T.
+    /// </returns>
     public static T? GetFirstOrDefaultFromStatement<T>(
         this IDbConnection connection,
         string commandText,
@@ -23,7 +55,42 @@ public static partial class DbConnectionExtensions
         var result = connection.QueryFirstOrDefault<T>(commandDefinition);
         return onExecuted is not null ? onExecuted(commandDefinition, result) : result;
     }
-    
+
+    /// <summary>
+    /// Executes a database statement (SQL command) and returns the first result as type T.
+    /// If a primitive type is required (int, string, etc.), then the first column of the first row will be returned.
+    /// If no results are found, the default value of T will be returned.
+    /// </summary>
+    /// <param name="connection">
+    /// The database connection to use for executing the statement.
+    /// </param>
+    /// <param name="commandText">
+    /// The SQL command text to execute.
+    /// </param>
+    /// <param name="parameters">
+    /// The parameters to pass to the command. This can be an anonymous object or a dictionary.
+    /// </param>
+    /// <param name="transaction">
+    /// An optional transaction to use for the command execution.
+    /// </param>
+    /// <param name="conventions">
+    /// The conventions to use for the command execution. If null, the default conventions will be used.
+    /// </param>
+    /// <param name="onExecuting">
+    /// An optional action to execute before the command is executed. This can be used from services to raise events or log the command.
+    /// </param>
+    /// <param name="onExecuted">
+    /// An optional action to execute after the command is executed. This can be used from services to raise events or log the command.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// An optional cancellation token for the asynchronous operation.
+    /// </param>
+    /// <typeparam name="T">
+    /// The type to map the result to.
+    /// </typeparam>
+    /// <returns>
+    /// The first result of the command execution as an instance of type T.
+    /// </returns>
     public static async Task<T?> GetFirstOrDefaultFromStatementAsync<T>(
         this IDbConnection connection,
         string commandText,
